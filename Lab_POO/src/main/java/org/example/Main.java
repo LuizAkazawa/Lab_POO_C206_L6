0c5d0119d1c5460c9f28b6613c47a0cb80cf1cab
Akazawa <luizakazawa@outlook.com>
2023-08-23 15:37:42 -0300

    aa

---
+++ Lab_POO/src/main/java/org/example/Main.java
@@ -0,0 +1,65 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Intro Java
+
+        //variaveis
+        //int valor;
+        //double saldo;
+
+        //constante
+        //int idade = 20;
+
+        /*
+        //String é uma classe no java
+        String nome = "AAA";
+        System.out.println(nome);
+        */
+        /*
+        double altura = 1.70;
+        float altura2;
+
+        altura2 = (float)altura;
+        System.out.println(altura2);
+        */
+
+        /*
+        Scanner cin = new Scanner(System.in);
+
+        System.out.println("Entre com sua idade: ");
+        int idade = cin.nextInt();
+
+        cin.nextLine(); //Funciona como o cin.ignore() --- gambiarra do java
+        System.out.println("Entre com seu nome: ");
+        String nomeAluno = cin.nextLine();
+
+        System.out.println("Entre com seu peso: ");
+        double peso = cin.nextDouble(); //double entra com "," ao invés de "."
+        */
+
+        /*
+        //variavel para if-else
+        int deposito = 100;
+        int transferencia = 100;
+        int saldo = deposito + transferencia;
+
+        if (saldo >= 200){
+            System.out.println("Voce consegue comprar");
+        }else{
+            System.out.println("Falta dinheiro");
+        }*/
+
+        /*
+        //estrutura FOR
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i+1);
+        }*/
+
+
+
+    }
+}
