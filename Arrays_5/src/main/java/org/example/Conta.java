cb9e132ab7985e7bd5473326a6ea543c29964761
Akazawa <luizakazawa@outlook.com>
2023-08-30 16:33:24 -0300

    Arrays in Java

---
+++ Arrays_5/src/main/java/org/example/Conta.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Conta {
+
+    //Atributos da classe
+    String dono;
+    double saldo;
+
+    public Conta(String dono, double saldo) {
+        this.dono = dono;
+        this.saldo = saldo;
+    }
+
+
+}
